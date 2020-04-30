de91ce5c19faa0c1bd2e6cd824837b795e0bda6f
Ikshita Srivastava <charvie19june@gmail.com>
2020-04-30 20:36:27 +0530

    vulnerability
    vulnerability

--- client/src/app/models/choice.ts
+++
@@ -1,5 +0,0 @@
-export class Choice {
-    constructor(public id: any, public text: string, public count: number) {
-
-    }
-}
--- client/src/app/models/survey.ts
+++
@@ -1,7 +0,0 @@
-
-import { Choice } from './choice';
-export class Survey {
-    constructor(public _id: any, public text: string, public choices: Array<Choice>) {
-
-    }
-}
