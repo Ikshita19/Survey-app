95117dc58383ac30636c9b04af7949feeba2c25e
Ikshita Srivastava <charvie19june@gmail.com>
2020-04-30 15:37:51 +0530

    push
    survey app

---
+++ client/src/app/models/choice.ts
@@ -0,0 +1,5 @@
+export class Choice {
+    constructor(public id: any, public text: string, public count: number) {
+
+    }
+}
---
+++ index.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// const MongoClient = require('mongodb').MongoClient;
+
+const app = express()
+const port = 3000
+
+// Connection URL
+const url = 'mongodb://localhost:27017/test';
+// Database Name
+
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+
+
+const SurveySchema = new mongoose.Schema({
+
+  text: { type: String, default: '' },
+  date: { type: Date, default: Date.now },
+  choices: [{
+    text: { type: String, default: '' },
+    count: { type: Number, default: 0 }
+  }]
+});
+const Survey = mongoose.model('Survey', SurveySchema);
+
+
+// parse application/json
+app.use(bodyParser.json())
+app.use(cors())
+app.use(express.static('client/dist/client'))
+
+app.get('/survey', (req, res) => {
+  Survey.find((err, result) => {
+    console.log(result);
+    res.json(result);
+  })
+})
+
+app.get('/survey/:id', (req, res) => {
+  Survey.findOne({ _id: req.params.id }, (err, result) => {
+    console.log(result.choices);
+    res.json(result);
+  })
+})
+
+app.post('/survey', (req, res) => {
+  let survey = req.body;
+  // users.push(user);
+  const obj = new Survey();
+  obj.text = survey.text
+  obj.choices = survey.choices
+  obj.save((error, result) => {
+    res.status(201).end();
+  })
+
+  //
+});
+
+app.delete('/survey/:id', (req, res) => {
+  Survey.findOneAndRemove({ _id: req.params.id }, (err, res) => {
+    if (err) {
+      req.flash("error", err);
+      res.end();
+    }
+
+
+    res.status(200).end();
+  });
+});
+
+// res.json(users)
+
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+module.exports = {
+  app
+};
+
+
+ // MongoClient.connect(url, function(err, client) {
+    //     console.log("Connected successfully to server");
+    //     const db = client.db(dbName);
+    //     const collection = db.collection('users');
+    //     collection.insertOne(user, (error, result)=>{
+    //         if(error) throw error;
+    //         console.log(result);
+    //         res.status(201).end();
+    //     });
+    //   });
